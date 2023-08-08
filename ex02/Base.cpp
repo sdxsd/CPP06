70756234c428ce948950f63eb4fd3f359e2d7782
sdxsd <willmaguire@icloud.com>
2023-08-08 17:08:56 +0200

    Base.cpp

--- ex02/Base.cpp
+++ ex02/Base.cpp
@@ -1,7 +1,29 @@
+#include <time.h>
+#include <stdlib.h>
+
 #include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base *(*generateInstance[3])(void) = {
+	[](void) -> Base* {
+		Base* x = new A;
+		return (x);
+	},
+	[](void) -> Base* {
+		Base* x = new B;
+		return (x);
+	},
+	[](void) -> Base* {
+		Base* x = new C;
+		return (x);
+	}
+};
 
 Base* generate(void) {
-	;
+	srand(time(NULL));
+	return (generateInstance[rand() % 2]());
 }
 
 void identify(Base* p) {
